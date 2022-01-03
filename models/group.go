6717d835360746ca112d9716a22c354eb62e49fb
Crayon <lsh328328@naver.com>
2022-01-03 10:01:27 +0900

    #4 [fix] : 그룹 영문명 입력 추가

--- models/group.go
+++ models/group.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/s-owl/sowl_manager_backend/firebaseapp"
@@ -12,14 +13,16 @@ const groupCollection = "groups"
 
 // GroupRegistInput - 그룹 등록 데이터
 type GroupRegistInput struct {
-	Name        string `json:"name" binding:"required"`
+	KoreanName  string `json:"koreanName" binding:"required"`
+	EnglishName string `json:"englishName" binding:"required"`
 	Description string `json:"description" binding:"required"`
 	Category    string `json:"category" binding:"required"`
 }
 
 // Group - Firestore에 저장할 그룹 데이터
 type Group struct {
-	Name        string    `json:"name"`
+	KoreanName  string    `json:"koreanName"`
+	EnglishName string    `json:"englishName"`
 	Description string    `json:"description"`
 	Category    string    `json:"category"`
 	Admin       string    `json:"admin"`
@@ -31,7 +34,25 @@ type Group struct {
 
 // Validate - 그룹 등록 데이터를 검증한다.
 func (group *GroupRegistInput) Validate() error {
-	if group.Name == "" || group.Description == "" || group.Category == "" {
+
+	// 그룹 국문명이 한글인지 확인
+	for _, value := range group.KoreanName {
+		if value < 44032 || value > 55203 {
+			fmt.Println(value)
+			return GroupKoreanNameError
+		}
+	}
+
+	// 그룹 영문명이 알파벳 소문자인지 확인
+	for _, value := range group.EnglishName {
+		if value < 97 || value > 122 {
+			fmt.Println(value)
+			return GroupEnglishNameError
+		}
+	}
+
+	// 비어있는 필드가 있으면 오류 반환
+	if group.KoreanName == "" || group.EnglishName == "" || group.Description == "" || group.Category == "" {
 		return GroupFieldNullError
 	}
 
@@ -44,7 +65,8 @@ func RegistGroup(context context.Context, groupInput *GroupRegistInput, userEmai
 	group = new(Group)
 
 	// GroupRegistInput에서 데이터 추출
-	group.Name = groupInput.Name
+	group.KoreanName = groupInput.KoreanName
+	group.EnglishName = groupInput.EnglishName
 	group.Description = groupInput.Description
 	group.Category = groupInput.Category
 	group.Admin = userEmail
@@ -52,7 +74,7 @@ func RegistGroup(context context.Context, groupInput *GroupRegistInput, userEmai
 	group.UpdatedAt = time.Now()
 
 	// Firestore에 저장 시도
-	_, err = firestoreClient.Collection(groupCollection).Doc(group.Name).Create(context, group)
+	_, err = firestoreClient.Collection(groupCollection).Doc(group.EnglishName).Create(context, group)
 	if err != nil {
 		group = nil
 		err = utils.FirestoreError(err)
@@ -61,4 +83,18 @@ func RegistGroup(context context.Context, groupInput *GroupRegistInput, userEmai
 	}
 
 	return
+}
+
+// GetGroupByEnglishName - 그룹 영문명으로 해당 그룹 조회
+func GetGroupByEnglishName(context context.Context, groupEnglishName string) (*Group, error) {
+	var group Group
+	firestoreClient := firebaseapp.App().Firestore
+
+	groupData, err := firestoreClient.Collection(groupCollection).Doc(groupEnglishName).Get(context)
+	if err != nil {
+	}
+
+	groupData.DataTo(&group)
+
+	return &group, nil
 }
--- models/groupError.go
+++ models/groupError.go
@@ -12,4 +12,16 @@ var (
 		Message:    "Please input all the information",
 		StatusCode: http.StatusBadRequest,
 	}
+
+	GroupKoreanNameError = &utils.AppError{
+		ErrorCode:  "G2",
+		Message:    "Group Korean Name can use only korean",
+		StatusCode: http.StatusBadRequest,
+	}
+
+	GroupEnglishNameError = &utils.AppError{
+		ErrorCode:  "G3",
+		Message:    "Group English name can use only lower case",
+		StatusCode: http.StatusBadRequest,
+	}
 )
